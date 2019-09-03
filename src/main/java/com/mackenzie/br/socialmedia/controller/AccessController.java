8c4425846042d3a3b09909d8c9106702dc6858a9
Matheus Lança <matheus.7910@hotmail.com>
2019-09-03 16:09:09 -0300

    Adicionando CrossOrigins para teste de comunicação com angular

--- src/main/java/com/mackenzie/br/socialmedia/controller/AccessController.java
+++ src/main/java/com/mackenzie/br/socialmedia/controller/AccessController.java
@@ -3,6 +3,7 @@ package com.mackenzie.br.socialmedia.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ public class AccessController {
 	@Autowired
 	private ProfessionalDAO professionalDAO;
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value = "/signUp", method = RequestMethod.POST)
 	public String signUp(@RequestBody ProfessionalDomain professional) {
 
