3228d9126a1beb03bf9a48fa6dec4f4988579a47
Guilherme Giazzi <p000ggiazzi@prservicos.com.br>
2019-09-12 22:20:13 -0300

    adicionando @Service no InterestTopicService

--- src/main/java/com/mackenzie/br/socialmedia/service/InterestTopicService.java
+++ src/main/java/com/mackenzie/br/socialmedia/service/InterestTopicService.java
@@ -3,10 +3,12 @@ package com.mackenzie.br.socialmedia.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mackenzie.br.socialmedia.DAO.InterestTopicDAO;
 import com.mackenzie.br.socialmedia.domain.InterestTopicDomain;
 
+@Service
 public class InterestTopicService {
 	
 	@Autowired
