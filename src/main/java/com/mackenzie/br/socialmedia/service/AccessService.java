513d6500a17d6c78509a68fc0e33013164a02559
Renan Biagiotti <renan.sao1@hotmail.com>
2019-10-23 22:06:25 -0300

    Alterando updateProfile

--- src/main/java/com/mackenzie/br/socialmedia/service/AccessService.java
+++ src/main/java/com/mackenzie/br/socialmedia/service/AccessService.java
@@ -11,125 +11,95 @@ import com.mackenzie.br.socialmedia.utils.ValidationUtils;
 
 @Service
 public class AccessService {
-	
+
 	@Autowired
 	private ProfessionalDAO professionalDAO;
-	
-	@Autowired 
+
+	@Autowired
 	private PaymentInfoDAO paymentInfoDAO;
-	
+
 	@Autowired
 	private ValidationUtils validationUtils;
 
-	public ProfessionalDomain signUp(ProfessionalDomain professional) throws IllegalArgumentException{
-		
+	public ProfessionalDomain signUp(ProfessionalDomain professional) throws IllegalArgumentException {
+
 		if (validateUser(professional.getUserLogin())) {
-			
+
 			throw new IllegalArgumentException("Usuário já em uso");
 		}
-		
-		ProfessionalDomain databaseProfessional = professionalDAO.insert(professional);	
+
+		ProfessionalDomain databaseProfessional = professionalDAO.insert(professional);
 		PaymentInfoDomain paymentInfo = professional.getPaymentInfo();
-		
-		if(paymentInfo != null) {
-			
+
+		if (paymentInfo != null) {
+
 			paymentInfo.setProfessionalID(databaseProfessional.getProfessionalID());
-			paymentInfoDAO.insert(paymentInfo);	
+			paymentInfoDAO.insert(paymentInfo);
 		}
-		
+
 		return databaseProfessional;
 	}
 
 	public ProfessionalDomain login(ProfessionalDomain professional) throws IllegalAccessException {
-		
-		validationUtils.validateProfessionalByUserLoginAndPassword(professionalDAO, professional.getUserLogin(), professional.getPassword());
-		
-		ProfessionalDomain databaseProfessional = professionalDAO.findByUserLoginAndPassword(professional.getUserLogin(), professional.getPassword());
-		
+
+		validationUtils.validateProfessionalByUserLoginAndPassword(professionalDAO, professional.getUserLogin(),
+				professional.getPassword());
+
+		ProfessionalDomain databaseProfessional = professionalDAO
+				.findByUserLoginAndPassword(professional.getUserLogin(), professional.getPassword());
+
 		return databaseProfessional;
 	}
 
-	public ProfessionalDomain updateProfile(ProfessionalDomain professional) throws IllegalArgumentException{
-		
+	public ProfessionalDomain updateProfile(ProfessionalDomain professional) throws IllegalArgumentException {
+
 		validationUtils.validateProfessionalByID(professionalDAO, professional.getProfessionalID());
-		
-		ProfessionalDomain databaseProfessional = professionalDAO.findByProfessionalID(professional.getProfessionalID());
-		
-		if (professional.getName() != null){
-			
-			databaseProfessional.setName(professional.getName());
-		}
-		
-		if (professional.getBirthDate() != null){
-		
-			databaseProfessional.setBirthDate(professional.getBirthDate());
-		}
-		
-		if (professional.getCareerDate() != null){
-		
-			databaseProfessional.setCareerDate(professional.getCareerDate());
-		}
-		
-		if (professional.getCity() != null){
-		
-			databaseProfessional.setCity(professional.getCity());
-		}
-		
-		if (professional.getInstructionLevel() != null){
-		
-			databaseProfessional.setInstructionLevel(professional.getInstructionLevel());
-		}
-		
-		if (professional.getJobRole() != null) {
-		
-			if (professional.getJobRole().getCompanyName() != null){
-			
-				databaseProfessional.getJobRole().setCompanyName(professional.getJobRole().getCompanyName());
-			}
-			
-			if (professional.getJobRole().getSalary() != 0){
-			
-				databaseProfessional.getJobRole().setSalary(professional.getJobRole().getSalary());
-			}
-		}
-		
-		if (professional.getPassword() != null){
-			databaseProfessional.setPassword(professional.getPassword());
-		}
-		
-		if (professional.getState() != null){
-		
-			databaseProfessional.setState(professional.getState());
-		}
-		
-		if (professional.getUserLogin() != null){
-			
-			databaseProfessional.setUserLogin(professional.getUserLogin());
-		}
+
+		ProfessionalDomain databaseProfessional = professionalDAO
+				.findByProfessionalID(professional.getProfessionalID());
+
+		databaseProfessional.setName(professional.getName());
+		databaseProfessional.setBirthDate(professional.getBirthDate());
+		databaseProfessional.setCareerDate(professional.getCareerDate());
+		databaseProfessional.setCity(professional.getCity());
+		databaseProfessional.setEmail(professional.getEmail());
+		databaseProfessional.setInstructionLevel(professional.getInstructionLevel());
+		databaseProfessional.getJobRole().setCompanyName(professional.getJobRole().getCompanyName());
+		databaseProfessional.getJobRole().setSalary(professional.getJobRole().getSalary());
+		databaseProfessional.getJobRole().setJobTitle(professional.getJobRole().getJobTitle());
+		databaseProfessional.setPassword(professional.getPassword());
+		databaseProfessional.setState(professional.getState());
+		databaseProfessional.setUserLogin(professional.getUserLogin());
+		databaseProfessional.getPaymentInfo().setCardName(professional.getPaymentInfo().getCardName());
+		databaseProfessional.getPaymentInfo().setCardNumber(professional.getPaymentInfo().getCardNumber());
+		databaseProfessional.getPaymentInfo().setCardSecurityCode(professional.getPaymentInfo().getCardSecurityCode());
+		databaseProfessional.getPaymentInfo().setCardValidationDate(professional.getPaymentInfo().getCardValidationDate().toString());
+		databaseProfessional.setProfileImage(professional.getProfileImage());
+		databaseProfessional.setProfileType(professional.getProfileType());
 		
 		professionalDAO.save(databaseProfessional);
 
 		return databaseProfessional;
 	}
 
-	public ProfessionalDomain retrieveProfessionalData(String professionalID) throws IllegalArgumentException{
-		
+	public ProfessionalDomain retrieveProfessionalData(String professionalID) throws IllegalArgumentException {
+
 		validationUtils.validateProfessionalByID(professionalDAO, professionalID);
-		
+
 		return professionalDAO.findByProfessionalID(professionalID);
 	}
 
 	public boolean validateUser(String userLogin) {
-		
-		for(ProfessionalDomain professional : professionalDAO.findAll()){
-			
+
+		for (ProfessionalDomain professional : professionalDAO.findAll()) {
+
 			if (professional.getUserLogin().equalsIgnoreCase(userLogin)) {
-			
+
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 }
